271fe8f55b51428887a25092f9f2f6c66fac89ac
gabrieladams <gabrieladams1738@gmail.com>
2020-07-16 19:37:52 +0300

    new updates

---
+++ build/precache-manifest.ae372d061e71522a65c2048a44657463.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dd3bf114d432a079f99816434d11b8a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "169c6b9d0c13ac21233c",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "36a224977d5acca06866",
+    "url": "/static/css/main.8413d844.chunk.css"
+  },
+  {
+    "revision": "169c6b9d0c13ac21233c",
+    "url": "/static/js/2.7686b808.chunk.js"
+  },
+  {
+    "revision": "618d9e067b5f7c0ee048a43ae1109b7d",
+    "url": "/static/js/2.7686b808.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36a224977d5acca06866",
+    "url": "/static/js/main.653dbf0d.chunk.js"
+  },
+  {
+    "revision": "53fa833537a6955966b2",
+    "url": "/static/js/runtime-main.35a26a64.js"
+  },
+  {
+    "revision": "f183017ca003bc9091a9fb610d7e8ab0",
+    "url": "/static/media/Ayuma-profile.f183017c.jpg"
+  },
+  {
+    "revision": "8fc1ec6c7831a0a5eeb70dbd16824c8a",
+    "url": "/static/media/Cheryl-profile.8fc1ec6c.jpg"
+  },
+  {
+    "revision": "d275c1a6fcde9524dece4d74cffe9da0",
+    "url": "/static/media/Jijo-profile.d275c1a6.jpg"
+  },
+  {
+    "revision": "2ee9cabdbba46d5a87fa4f9cd3fb7120",
+    "url": "/static/media/Joan-profile.2ee9cabd.jpg"
+  },
+  {
+    "revision": "f5f2946c975628cacb50783bee0f7e5f",
+    "url": "/static/media/Wambida-profile.f5f2946c.jpg"
+  },
+  {
+    "revision": "d445e7e232f05468a214f8fcc51014f3",
+    "url": "/static/media/art-performance-and-talent-showcase.d445e7e2.jpg"
+  },
+  {
+    "revision": "ebcc18b63a1e4bd1474a3ba3b24a50be",
+    "url": "/static/media/co-founder.ebcc18b6.jpg"
+  },
+  {
+    "revision": "f9dd698c19d3a5db8d4d48d053613eed",
+    "url": "/static/media/community-capacity-building-and-emp.f9dd698c.jpg"
+  },
+  {
+    "revision": "5f1f490b6b3fae3e94b952f005949e43",
+    "url": "/static/media/community-projects.5f1f490b.jpg"
+  },
+  {
+    "revision": "505165569a67097ee24a9299e3a000cc",
+    "url": "/static/media/community-services.50516556.jpg"
+  },
+  {
+    "revision": "3cbc49705fda43352ffe39e72cb9bd51",
+    "url": "/static/media/donation-volunteer-hero.3cbc4970.jpg"
+  },
+  {
+    "revision": "6630705dc555fba065f8ba959cb20a0d",
+    "url": "/static/media/events.6630705d.jpg"
+  },
+  {
+    "revision": "a300769e6bf96c5c2c4a5cd58f95fa76",
+    "url": "/static/media/gallery-img-1.a300769e.jpg"
+  },
+  {
+    "revision": "607a8d9aa51eaf0d8fa669049d85ab36",
+    "url": "/static/media/gallery-img-2.607a8d9a.jpg"
+  },
+  {
+    "revision": "c4e757d3aa957edb4b10e8410309f38f",
+    "url": "/static/media/gallery-img-3.c4e757d3.jpg"
+  },
+  {
+    "revision": "9c9d0ba9579344521e48ff1f2789f58f",
+    "url": "/static/media/gallery-img-4.9c9d0ba9.jpg"
+  },
+  {
+    "revision": "defd4ca761a7f40a9509d9959436cc73",
+    "url": "/static/media/gallery-img-5.defd4ca7.jpg"
+  },
+  {
+    "revision": "11c49aff95eda30ae2d3bb137eec79e8",
+    "url": "/static/media/gallery-img-6.11c49aff.jpg"
+  },
+  {
+    "revision": "2025a135f0d612de4cd65125c4e755c7",
+    "url": "/static/media/hero.2025a135.jpg"
+  },
+  {
+    "revision": "248d7107efb7ee18d38f1799f6d59d9d",
+    "url": "/static/media/kawangware-initiative-project.248d7107.jpg"
+  },
+  {
+    "revision": "aad7bee90467c6e3688aeb6d8ddc9ae3",
+    "url": "/static/media/mentorship-programmes.aad7bee9.jpg"
+  },
+  {
+    "revision": "428fa72a2faa722e8ae384e9a5e08947",
+    "url": "/static/media/talent-search-and-nurturing.428fa72a.jpg"
+  }
+]);
