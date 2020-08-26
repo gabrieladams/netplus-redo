9d7f0def6911c2379b0c7b8eb45e0320effb80ad
gabrieladams <gabrieladams1738@gmail.com>
2020-08-26 18:53:23 +0300

    new commit

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -7,30 +7,19 @@ function Contact() {
   return (
     <React.Fragment>
       {/* <Form /> */}
-      <NetlifyForm
-        name='Form With Recaptcha'
-        recaptcha={{
-          sitekey: 'my_recaptcha_site_key',
-          size: 'normal',
-        }}
-      >
-        {({ loading, error, recaptchaError, success, recaptcha }) => (
+      import NetlifyForm from 'react-netlify-form'
+      <NetlifyForm name='Contact Form'>
+        {({ loading, error, success }) => (
           <div>
             {loading && <div>Loading...</div>}
             {error && (
               <div>Your information was not sent. Please try again later.</div>
-            )}
-            {recaptchaError && (
-              <div>
-                Recaptcha did not match. Please make sure the box is checked.
-              </div>
             )}
             {success && <div>Thank you for contacting us!</div>}
             {!loading && !success && (
               <div>
                 <input type='text' name='Name' required />
                 <textarea name='Message' required />
-                {recaptcha}
                 <button>Submit</button>
               </div>
             )}
